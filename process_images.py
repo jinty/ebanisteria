a023b030545c575644cbdfc1dc71ba9349214c3f
Brian Sutherland <brian@vanguardistas.net>
2017-06-09 22:38:33 +0200

    fix orientation and remove 2400w image

--- process_images.py
+++ process_images.py
@@ -9,7 +9,7 @@ for file in os.listdir('original_images'):
         continue
     path = os.path.join(dir, file)
     dest = 'static/img/proj/{}-' + file
-    for width in [150, 300, 600, 900, 1200, 1800, 2400]:
+    for width in [150, 300, 600, 900, 1200, 1800]:
         print('making ', dest.format(width))
-        check_call(['convert', path, '-resize', str(width), '-strip', '-quality', '90', dest.format(width)])
+        check_call(['convert', path, '-auto-orient', '-resize', str(width), '-strip', '-quality', '90', dest.format(width)])
 
